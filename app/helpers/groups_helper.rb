bd12f7fdda8acaf546450e0a90005d734a5aa28f
umair-rao <umairmuqeem@gmail.com>
2024-02-10 14:02:08 +0500

    Update HELPER

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,5 +1,11 @@
 module GroupHelper
-  def total_amount(group)
-    group.purchases.sum(:amount)
+  
+  def check_url(url)
+    default = 'missing_avatar.png'
+    if ((File.extname(url) =~/^(.png|.gif|.jpg)$/ )||(url =~ /^#{URI::regexp}$/))
+      url
+    else
+      default
+    end
   end
 end
