6d6f8586890754adefe18ba4f75f93792384a8e5
umair-rao <umairmuqeem@gmail.com>
2024-02-10 12:18:50 +0500

    Fix Linters & Create views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,13 @@
 class GroupsController < ApplicationController
-    def index; end
+  def index; end
 
-    def show; end
-  
-    def new; end
-  
-    def edit; end
-  
-    def update; end
-  
-    def delete; end
+  def show; end
+
+  def new; end
+
+  def edit; end
+
+  def update; end
+
+  def delete; end
 end
